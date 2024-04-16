22679a72f05d9bdc0ec85b4ffbbcb922978c8e5c
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-04-16 17:44:39 +0200

    ADD: flag

--- src/components/CardApp.vue
+++ src/components/CardApp.vue
@@ -4,7 +4,7 @@
             <li >
                 <h1>{{movie.title}}</h1>
                 <h2>{{movie.original_title}}</h2>
-                <h3>{{movie.language}}</h3>
+                <h3 :class="movie.language" >.</h3>
                 <h5>{{movie.vote}}</h5>
             </li>
             
@@ -24,5 +24,25 @@ import {store} from '../store.js'
 </script>
 
 <style lang="scss" scoped>
-
+.en{
+    // background: blue;
+    background-image: url(../../public/img/england.png);
+    background-size: cover;
+    border-radius: 5px;
+    width: 50px;
+}
+.it{
+    // background: green;
+    background-image: url(../../public/img/italy.png);
+    border-radius: 5px;
+    background-size: cover;
+    width: 50px;
+}
+.es{
+    // background: red;
+    background-image: url(../../public/img/spanish.png);
+    background-size: cover;
+    border-radius: 5px;
+    width: 50px;
+}
 </style>
